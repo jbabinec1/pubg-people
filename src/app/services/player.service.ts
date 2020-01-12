eccd5636d45f468f753e5e6cedd1b3409282231c
Jared <jbabinec1@gmail.com>
2020-01-12 12:58:17 -0500

    env stuff

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -23,7 +23,7 @@ export class PlayerService {
 
   getPlayer(player: string):Observable<Player[]> {
 
-    const api_key = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4MDUzZmEyMC02MzhjLTAxMzctMGNlYi0wMGQxMWQwYzg3MzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTU5MDU3ODgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImpiYWJpbmVjMS1nbWFpIn0.LI-UQ8XiwVQ-vpbE5nmPzbe0sLj7ROJjpPGgXQHRuug';
+    const api_key = process.env.API_KEY;
 
     let getHeaders = new HttpHeaders({'Authorization': api_key, 'Accept': 'application/vnd.api+json'}); 
   
@@ -37,7 +37,7 @@ export class PlayerService {
 
       getSeasonStats(id: string):Observable<SeasonStats[]> {
 
-        const api_key = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4MDUzZmEyMC02MzhjLTAxMzctMGNlYi0wMGQxMWQwYzg3MzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTU5MDU3ODgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImpiYWJpbmVjMS1nbWFpIn0.LI-UQ8XiwVQ-vpbE5nmPzbe0sLj7ROJjpPGgXQHRuug';
+        const api_key = process.env.API_KEY;
 
         let getHeaders = new HttpHeaders({'Authorization': api_key, 'Accept': 'application/vnd.api+json'}); 
       
