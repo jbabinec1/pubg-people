623f253c4fd765b0c3e762fd322b263bd4e716e6
Jared <jbabinec1@gmail.com>
2020-01-11 20:08:18 -0500

    almost

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-//const port = process.env.PORT || port
+app.use(express.static('src'))
+
+const port = process.env.PORT || 3000; 
 
 app.get('/api', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || port, function(){
+app.listen(port, function(){
     console.log('Your node js server is running');
 });
