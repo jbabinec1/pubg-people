37da3949fc8cc2d92f8c75818632ebe0192d68b1
Jared <jbabinec1@gmail.com>
2020-01-13 21:00:57 -0500

    update server

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -42,8 +42,10 @@ export class PlayerService {
     //const api_key = process.env.API_KEY;
 
     //let getHeaders = new HttpHeaders({ 'Accept': 'application/vnd.api+json'});
+
+    let getHeaders = new HttpHeaders({'Accept': 'application/vnd.api+json'}); 
   
-    return this.http.get<Player[]> (`https://pubg-people.herokuapp.com/players/${player}`,  { observe:'body',   responseType: 'json' }).pipe(share()); 
+    return this.http.get<Player[]> (`/players/${player}`,  { observe:'body',   responseType: 'json', headers: getHeaders }).pipe(share()); 
   
       } 
 
