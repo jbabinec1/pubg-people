6a22abc8df51361575a364ee6ceb65ae5e71fbe4
Jared <jbabinec1@gmail.com>
2020-01-14 21:59:50 -0500

    async request test

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ var router = express.Router();
 //let data = "";
 
 
-// Make request to get ID property of player (steam)
-
+// Make request to get ID property of player (steam) ORIGINAL COPY 
+/*
  app.get('/players/:player',  function(request, response) {
 
-    //const player = request.params.player;
      const player = request.params.player;
      const api_url = `https://api.pubg.com/shards/steam/players?filter[playerNames]=${player}`;
      const stats_url = `https://api.pubg.com/shards/steam/players/${stat}/seasons/division.bro.official.pc-2018-05`;
@@ -54,7 +53,42 @@ var router = express.Router();
 
     apiRequest.end();
 
-     }) /* end of player id request */
+
+     }) /* end of player id request (app.get) ORIGINAL */
+
+
+
+
+
+/* ASYNC TEST  */
+     app.get('/players/:player',  async (request, response) => {
+
+        const player = request.params.player;
+        const api_url = `https://api.pubg.com/shards/steam/players?filter[playerNames]=${player}`;
+        const stats_url = `https://api.pubg.com/shards/steam/players/${stat}/seasons/division.bro.official.pc-2018-05`;
+        const stat = request.params.stat;
+        
+        var options = {
+   
+           method: "GET",
+           observe: 'body',
+           responseType: 'json',
+           headers: {
+               "authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4MDUzZmEyMC02MzhjLTAxMzctMGNlYi0wMGQxMWQwYzg3MzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTU5MDU3ODgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImpiYWJpbmVjMS1nbWFpIn0.LI-UQ8XiwVQ-vpbE5nmPzbe0sLj7ROJjpPGgXQHRuug',
+               "accept": 'application/vnd.api+json' }
+           };
+   
+        const data = "";
+   
+       const apiRequest = await fetch (api_url, options)
+       const json = await apiRequest.json(); 
+       response.json(json);
+   
+       })
+   
+   
+   
+         /* end of player id request (app.get) */
 
 
 
