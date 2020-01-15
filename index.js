0dd77fdc953756c132f00d69755aa4ba87a0e710
Jared <jbabinec1@gmail.com>
2020-01-14 19:57:08 -0500

    deleted duplicate response end and made data string a global variable

--- index.js
+++ index.js
@@ -9,44 +9,7 @@ var router = express.Router();
 
 
 
-
-
-  /* Get seasons stats for player endpoint   */
-  app.get('players/stats/:stat',  function(request, response) {
-
-    //const player = request.params.player;
-     const stat = request.params.stat;
-     const api_url = `https://api.pubg.com/shards/steam/players/${stat}/seasons/division.bro.official.pc-2018-05`;
-     
-     var options = {
-
-        method: "GET",
-        observe: 'body',
-        responseType: 'json',
-        headers: {
-            "authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4MDUzZmEyMC02MzhjLTAxMzctMGNlYi0wMGQxMWQwYzg3MzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTU5MDU3ODgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImpiYWJpbmVjMS1nbWFpIn0.LI-UQ8XiwVQ-vpbE5nmPzbe0sLj7ROJjpPGgXQHRuug',
-            "accept": 'application/vnd.api+json' }
-        };
- 
-        let data = "";
-
-        let playerStatsRequest = http.request(api_url, options, function (res) {
-    
-            console.log("connected sonion");
-    
-            res.on("data", chunk => {
-                data += chunk;
-            })
-    
-            res.on("end", () => {
-                console.log("data collect");
-    
-               // response.end(JSON.stringify(data));
-            })
-    
-        });
-        playerStatsRequest.end();
-    }); 
+let data = "";
 
 
 // Make request to get ID property of player (steam)
@@ -68,7 +31,7 @@ var router = express.Router();
         };
 
 
-    let data = "";
+  //  let data = "";
 
     let apiRequest = http.request(api_url, options, function (res) {
 
@@ -78,11 +41,11 @@ var router = express.Router();
             data += chunk;
         })
 
-        res.on("end", () => {
+      /*  res.on("end", () => {
             console.log("data collect");
 
-            response.end(JSON.stringify(data));
-        })
+           response.end(JSON.stringify(data));
+        }) */
 
     })
 
@@ -93,8 +56,43 @@ var router = express.Router();
 
 
 
+    /* Get seasons stats for player endpoint   */
 
+     app.get('/players/stats/:stat',  function(request, response) {
 
+        //const player = request.params.player;
+         const stat = request.params.stat;
+         const api_url = `https://api.pubg.com/shards/steam/players/${stat}/seasons/division.bro.official.pc-2018-05`;
+         
+         var options = {
+    
+            method: "GET",
+            observe: 'body',
+            responseType: 'json',
+            headers: {
+                "authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4MDUzZmEyMC02MzhjLTAxMzctMGNlYi0wMGQxMWQwYzg3MzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTU5MDU3ODgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImpiYWJpbmVjMS1nbWFpIn0.LI-UQ8XiwVQ-vpbE5nmPzbe0sLj7ROJjpPGgXQHRuug',
+                "accept": 'application/vnd.api+json' }
+            };
+     
+            //let data = "";
+    
+            let playerStatsRequest = http.request(api_url, options, function (res) {
+        
+                console.log("connected sonion");
+        
+                res.on("data", chunk => {
+                    data += chunk;
+                })
+        
+                res.on("end", () => {
+                    console.log("data collect");
+        
+                    response.end(JSON.stringify(data));
+                })
+        
+            });
+            playerStatsRequest.end();
+        }); 
 
 
   
