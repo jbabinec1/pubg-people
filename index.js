db93a17aba6c385aaca2f62071d0c3918bd87b3c
Jared <jbabinec1@gmail.com>
2020-01-14 22:25:10 -0500

    back to square one

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var router = express.Router();
 
 
 // Make request to get ID property of player (steam) ORIGINAL COPY 
-/*
+
  app.get('/players/:player',  function(request, response) {
 
      const player = request.params.player;
@@ -35,6 +35,7 @@ var router = express.Router();
 
     let apiRequest = http.request(api_url, options, function (res) {
 
+        
         res.on("data", chunk => {
             data += chunk;
         })
@@ -60,31 +61,7 @@ var router = express.Router();
 
 
 
-/* ASYNC TEST  */
-     app.get('/players/:player',  async (request, response) => {
 
-        const player = request.params.player;
-        const api_url = `https://api.pubg.com/shards/steam/players?filter[playerNames]=${player}`;
-        const stats_url = `https://api.pubg.com/shards/steam/players/${stat}/seasons/division.bro.official.pc-2018-05`;
-        const stat = request.params.stat;
-        
-        var options = {
-   
-           method: "GET",
-           observe: 'body',
-           responseType: 'json',
-           headers: {
-               "authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4MDUzZmEyMC02MzhjLTAxMzctMGNlYi0wMGQxMWQwYzg3MzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTU5MDU3ODgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImpiYWJpbmVjMS1nbWFpIn0.LI-UQ8XiwVQ-vpbE5nmPzbe0sLj7ROJjpPGgXQHRuug',
-               "accept": 'application/vnd.api+json' }
-           };
-   
-        const data = "";
-   
-       const apiRequest = await fetch (api_url, options)
-       const json = await apiRequest.json(); 
-       response.json(json);
-   
-       })
    
    
    
