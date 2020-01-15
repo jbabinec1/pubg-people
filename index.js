06accab6a08bc4922eacf49934cc667911635419
Jared <jbabinec1@gmail.com>
2020-01-14 22:59:09 -0500

    s

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const fetch = require('node-fetch');
-const http = require('https');
+//const fetch = require('node-fetch');
+const http = require('http');
 var router = express.Router();
 
 //const API_KEY = process.env.API_KEY;
@@ -14,8 +14,7 @@ var router = express.Router();
 
      let player = request.params.player;
      let api_url = `https://api.pubg.com/shards/steam/players?filter[playerNames]=${player}`;
-     const stats_url = `https://api.pubg.com/shards/steam/players/${stat}/seasons/division.bro.official.pc-2018-05`;
-     let stat = request.params.stat;
+     
      
      var options = {
 
@@ -41,7 +40,7 @@ var router = express.Router();
          res.on("end", () => {
             console.log("data collect");
 
-           response.end(JSON.stringify(data));
+          response.end(JSON.stringify(data));
     
         })
 
@@ -49,54 +48,7 @@ var router = express.Router();
 
     apiRequest.end();
 
-
      }) /* end of player id request (app.get) ORIGINAL */
-
-
-
-
-
-
-    /*
-
-     app.get('/players/:stat',  function(request, response) {
-
-        //const player = request.params.player;
-         const stat = request.params.stat;
-         const stats_url = `https://api.pubg.com/shards/steam/players/${stat}/seasons/division.bro.official.pc-2018-05`;
-         
-         var options = {
-    
-            method: "GET",
-            observe: 'body',
-            responseType: 'json',
-            headers: {
-                "authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4MDUzZmEyMC02MzhjLTAxMzctMGNlYi0wMGQxMWQwYzg3MzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTU5MDU3ODgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImpiYWJpbmVjMS1nbWFpIn0.LI-UQ8XiwVQ-vpbE5nmPzbe0sLj7ROJjpPGgXQHRuug',
-                "accept": 'application/vnd.api+json' }
-            };
-     
-            //let data = "";
-    
-            let playerStatsRequest = http.request(stats_url, options, function (res) {
-        
-            
-        
-                res.on("data", chunk => {
-                    data += chunk;
-                })
-        
-                res.on("end", () => {
-                    console.log("data collect");
-        
-                    res.end(JSON.stringify(data));
-                })
-        
-            });
-            
-            playerStatsRequest.end();
-        }); 
-
-        */
 
   
 
