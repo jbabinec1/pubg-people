d88a4b88551254496a5a1552aa1ff9ac319a6cdd
Jared <jbabinec1@gmail.com>
2020-01-19 19:51:44 -0500

    limiter test

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ app.get('/players/:player', function(request, res) {
     }) */
 
     const limiter = rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
+        windowMs: 4 * 60 * 1000, // 15 minutes
         max: 9 // limit each IP to 100 requests per windowMs
       });
 
