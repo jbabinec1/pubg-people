d0d9956333d117af3c10747ad1312166a016a481
Jared <jbabinec1@gmail.com>
2020-01-22 08:37:50 -0500

    add / in front of dist

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ app.use(express.static('dist/pubg-app'));
 const port = process.env.PORT || 3000; 
 
 app.get('/*', function(req, res) {
-   res.sendFile(path.join(__dirname, 'dist/pubg-app/index.html'));
+   res.sendFile(path.join(__dirname, '/dist/pubg-app/index.html'));
 }); 
 
 app.listen(port, function(){
