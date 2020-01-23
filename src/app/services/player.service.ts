29381bb3d40c901fcecf32b9750ad826400bf0ca
Jared <jbabinec1@gmail.com>
2020-01-22 19:38:31 -0500

    with endpoint

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -20,7 +20,7 @@ export class PlayerService {
   constructor(private http: HttpClient) { }
 
 
-/* Testing version of getPlayer until I get the proxy working for testing */
+/* Testing version of getPlayer until I get the proxy working for testing 
 
  getPlayer(player: string):Observable<Player[]> {
 
@@ -33,12 +33,12 @@ export class PlayerService {
   
       return this.http.get<Player[]> (`https://api.pubg.com/shards/steam/players?filter[playerNames]=${player}`,  { observe:'body',   responseType: 'json', headers: getHeaders, }).pipe(share());    
   
-      }  
+      }  */
 
 
 
 
-      /* Makes request to my endpoint that makes a request to the PUBG API  
+      /* Makes request to my endpoint that makes a request to the PUBG API  */
 
   getPlayer(player: string):Observable<Player[]> {
 
@@ -49,53 +49,54 @@ export class PlayerService {
   
     return this.http.get<Player[]> (`/players/${player}`,  { observe:'body',   responseType: 'json', headers: getHeaders }).pipe(share()); 
   
-      }  */
+      } 
 
 
 
-      /* GET SEASON 5 STATS HIDING KEY 
+      /* GET SEASON 5 STATS HIDING KEY */
       getSeasonStats(id: string):Observable<SeasonStats[]> {
 
         //const api_key = process.env.API_KEY;  
         let getHeaders = new HttpHeaders({'Accept': 'application/vnd.api+json', 'Cache-Control': 'no-cache'}); 
           return this.http.get<SeasonStats[]>(`/player/${id}`, {observe:'body', responseType: 'json', headers: getHeaders }).pipe(share());
              
-        }   */
+        }   
 
    
 
-    /*    Testing version of getSeasonStats until I can get my proxy working */
+    /*    Testing version of getSeasonStats until I can get my proxy working 
 
         getSeasonStats(id: string):Observable<SeasonStats[]> {
 
           let getHeaders = new HttpHeaders({'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4MDUzZmEyMC02MzhjLTAxMzctMGNlYi0wMGQxMWQwYzg3MzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTU5MDU3ODgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImpiYWJpbmVjMS1nbWFpIn0.LI-UQ8XiwVQ-vpbE5nmPzbe0sLj7ROJjpPGgXQHRuug',
           "accept": 'application/vnd.api+json', 'Accept': 'application/vnd.api+json'}); 
             return this.http.get<SeasonStats[]>(`https://api.pubg.com/shards/steam/players/${id}/seasons/division.bro.official.pc-2018-05`,  { observe:'body',   responseType: 'json', headers: getHeaders });       
-          } 
+          } */
 
 
 
 
 
-/*
+
         getSeasonFourStats(id: string):Observable<SeasonStats[]> {
 
           let getHeaders = new HttpHeaders({'Authorization':'API_KEY', 'Accept': 'application/vnd.api+json'}); 
         
             return this.http.get<SeasonStats[]>(`/season4/${id}`,  { observe:'body',   responseType: 'json', headers: getHeaders });       
     
-          }  */
+          }  
 
 
 
+
+          /*
           getSeasonFourStats(id: string):Observable<SeasonStats[]> {
 
             let getHeaders = new HttpHeaders({'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4MDUzZmEyMC02MzhjLTAxMzctMGNlYi0wMGQxMWQwYzg3MzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTU5MDU3ODgxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImpiYWJpbmVjMS1nbWFpIn0.LI-UQ8XiwVQ-vpbE5nmPzbe0sLj7ROJjpPGgXQHRuug', 'Accept': 'application/vnd.api+json'}); 
           
               return this.http.get<SeasonStats[]>(`https://api.pubg.com/shards/steam/players/${id}/seasons/division.bro.official.pc-2018-04`,  { observe:'body',   responseType: 'json', headers: getHeaders });     
 
-
-          }  
+          }  */
 
 
 
