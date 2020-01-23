4d67ca1079ba6f538ed4b1b21c822748ddd4f1d1
Jared <jbabinec1@gmail.com>
2020-01-22 22:16:54 -0500

    change player route

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.use((req, res, next) => {
     res.status(error.status || 500).send({
       error: {
         status: error.status || 500,
-        message: error.message || 'Internal Server Error',
+        message: errors["0"].title || 'Internal Server Error',
       },
     });
   });
