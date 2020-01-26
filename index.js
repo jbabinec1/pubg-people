656cb8a0e2e84a8ae535fb0a78ff20e10815a5d5
Jared <jbabinec1@gmail.com>
2020-01-25 19:05:29 -0500

    auth

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const apiLimiter = rateLimit({
 
 app.get('/players/:player', apiLimiter, function(request, response) {
 
-   var key = process.env.API_KEY;
+   var key = process.env.API_AUTH;
     
     const player = request.params.player;
     const api_url = `https://api.pubg.com/shards/steam/players?filter[playerNames]=${player}`;
@@ -32,7 +32,7 @@ app.get('/players/:player', apiLimiter, function(request, response) {
        observe: 'body',
        responseType: 'json',
        headers: {
-           "authorization": key }     
+           "authorization": `Bearer ${key}`}     
 
        };
 
@@ -61,7 +61,7 @@ app.get('/players/:player', apiLimiter, function(request, response) {
 
     app.get('/player/:id', function(request, response, next) {
 
-      var key = process.env.API_KEY;
+      var key = process.env.API_AUTH;
 
        const id = request.params.id;
        const stats_url = `https://api.pubg.com/shards/steam/players/${id}/seasons/division.bro.official.pc-2018-05`;
@@ -72,7 +72,7 @@ app.get('/players/:player', apiLimiter, function(request, response) {
           observe: 'body',
           responseType: 'json',
           headers: {
-              "authorization": key }
+              "authorization": `Bearer ${key}` }
           };
   
      let data = "";
