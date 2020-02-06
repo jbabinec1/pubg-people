258d2b8b83a4cf8a692e6a48d41eb76666d6b2df
Jared <jbabinec1@gmail.com>
2020-02-06 01:47:18 -0500

    error handling

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.get('/players/:player', apiLimiter, function(request, response) {
               //let objectParsed = JSON.parse(data);
                let objectParsed = JSON.parse(JSON.stringify(data));
                
-               if ( typeof objectParsed.errors === "undefined" ) {
+               if ( objectParsed.hasOwnPropery('title')) {
                 response.status(404).send("No data for that player name.");
             } else {
                 response.send(objectParsed);
