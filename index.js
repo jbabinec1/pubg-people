2d4cbe5f9ebc60ab7946e717dc347565f25c7dee
Jared <jbabinec1@gmail.com>
2020-02-09 20:37:41 -0500

    add local testing

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ app.use((req, res, next) => {
     });
   });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 app.listen(port, function(){
    console.log('Your node js server is running');
 });
